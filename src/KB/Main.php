defd6b44101499317162962579e9e648bc13a528
Skullex <jross3740@gmail.com>
2016-02-15 08:34:37 -0500

    Added new stuff :3

--- src/KB/Main.php
+++ src/KB/Main.php
@@ -5,29 +5,43 @@ namespace KB;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\level\sound\BlazeShootSound;
-use pocketmine\event\player\EntityDamageEvent;
-use pocketmine\event\player\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\Entity;
 
-class KnockBack extends PluginBase implements Listener {
+class KnockBack extends PluginBase implements Listener{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-	public function onLoad(){
-		$this->getLogger()->info("KnockBack §aenabled");
-	}
+    public function onLoad(){
+        $this->getLogger()->info("KnockBack by Skullex §aenabled :D");
+    }
 
-    public function onDamage(EntityDamageEvent $event){
-        $entity = $event->getEntity();
-    if($event instanceof EntityDamageByEntityEvent){
-        $event->setKnockBack(1,0,1);
-        $fizz = new BlazeShootSound($entity);
-        $entity->getLevel()->addSound($fizz);
+            public function onDamage(EntityDamageEvent $event){
+            $entity = $event->getEntity();
+            if($event instanceof EntityDamageByEntityEvent){
+          $fizz = new BlazeShootSound($entity);
+          $entity->getLevel()->addSound($fizz);
+      if($entity->getDirection() == 0){
+        $entity->knockBack($entity, 0, 1, 0, 1);
+      }
+      elseif($entity->getDirection() == 1){
+        $entity->knockBack($entity, 0, 0, 1, 1);
+      }
+      elseif($entity->getDirection() == 2){
+        $entity->knockBack($entity, 0, -1, 0, 1);
+      }
+      elseif($entity->getDirection() == 3){
+        $entity->knockBack($entity, 0, 0, -1, 1);
+      }    
+$entity->sendTIP("§aYou have been launched");
+    }
 }
 
     public function onDisable(){
-        $this->getLogger()->info("KnockBack §cdisabled.");
+        $this->getLogger()->info("KnockBack by Skullex §cdisabled D:");
         return true;
-   	}
+    }
 }
