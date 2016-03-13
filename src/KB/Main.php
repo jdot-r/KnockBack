a0e9ba6dac31564c535ddb2ae1d15c2aa2724555
Skullex <jross3740@gmail.com>
2016-03-13 01:04:22 -0500

    Update v2.0.0
    This update is a test, if proven successful then the current modifications will remain unchanged!!

--- src/KB/Main.php
+++ src/KB/Main.php
@@ -3,47 +3,46 @@
 namespace KB;
 
 use pocketmine\plugin\PluginBase;
+
 use pocketmine\event\Listener;
+
 use pocketmine\level\sound\BlazeShootSound;
+
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+
 use pocketmine\event\entity\EntityDamageEvent;
+
 use pocketmine\Entity;
-use pocketmine\utils\TextFormat;
+
+use pocketmine\utils\TextFormat as Color;
 
 class KnockBack extends PluginBase implements Listener{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
     }
-
+    
     public function onLoad(){
-        $this->getLogger()->info(TextFormat::BOLD."KnockBack by Skullex enabled :D");
+        $this->getLogger()->info(Color::GREEN ."KnockBack has been successfully loaded!");
     }
-
-            public function onDamage(EntityDamageEvent $event){
-            $entity = $event->getEntity();
-            $player = $event->getPlayer();
-            if($event instanceof EntityDamageByEntityEvent){
-          $fizz = new BlazeShootSound($entity);
-          $entity->getLevel()->addSound($fizz);
-      if($entity->getDirection() == 0){
-        $entity->knockBack($entity, 0, 1, 0, 1);
-      }
-      elseif($entity->getDirection() == 1){
-        $entity->knockBack($entity, 0, 0, 1, 1);
-      }
-      elseif($entity->getDirection() == 2){
-        $entity->knockBack($entity, 0, -1, 0, 1);
-      }
-      elseif($entity->getDirection() == 3){
-        $entity->knockBack($entity, 0, 0, -1, 1);
-      }    
-$event->getPlayer()->sendTIP("You have been launched");
-    }
-}
-
-    public function onDisable(){
-        $this->getLogger()->info(TextFormat::RED."KnockBack by Skullex disabled D:");
-        return true;
-    }
-}
+    
+    public function onDamage(EntityDamageEvent $event){
+        $entity = $event->getEntity();
+        $player = $event->getPlayer();
+        //Level or Level's in which knockback will be activated! Testing if this will work :P
+        if($player->getLevel($this->getConfig()->get("Level"));
+        if($event instanceof EntityDamageByEntityEvent){
+            $fizz = new BlazeShootSound($entity);
+            $entity->getLevel()->addSound($fizz);
+            $event->setknockBack($this->getConfig()->get("Power"));
+            $event->getPlayer()->sendTip(   Color::GREEN ."You have been launched");
+        }
+           }
+           }
+           }
+           
+           public function onDisable(){
+               $this->getLogger()->info(TextFormat::RED ."KnockBack has been successfully unloaded!");
+               return true;
+           }
